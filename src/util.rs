2e9c5db86d43f4b313982631c834d62aabb7d7aa
Michael Hinterdorfer <20180021@students.htl-perg.ac.at>
2023-02-03 15:34:17 +0100

    #10 implemented random double

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,4 @@
 use std::{
-    error::Error,
     fs::File,
     io::{BufWriter, Write},
 };
@@ -22,4 +21,13 @@ pub const PI: f64 = std::f64::consts::PI;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
+}
+
+pub fn rand() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0.0..1.0)
+}
+
+pub fn random_double(min: f64, max: f64) -> f64 {
+    min + (max - min) * rand()
 }
