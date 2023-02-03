e069bcf3a5d7eb734ccb94af24d577c06548b382
Michael Hinterdorfer <20180021@students.htl-perg.ac.at>
2023-02-03 16:28:44 +0100

    closes #10 finished chapter antialiasing

--- src/camera.rs
+++ src/camera.rs
@@ -1,14 +1,14 @@
-use crate::vec3::Vec3;
+use crate::{ray::Ray, vec3::Vec3};
 
 pub struct Camera {
     pub origin: Vec3,
     pub lower_left_corner: Vec3,
     pub horizontal: Vec3,
-    pub verticcal: Vec3,
+    pub vertical: Vec3,
 }
 
 impl Camera {
-    fn new() -> Camera {
+    pub fn new() -> Camera {
         let aspect_ratio = 16.0 / 9.0;
         let viewport_height = 2.0;
         let viewport_width = aspect_ratio * viewport_height;
@@ -24,7 +24,14 @@ impl Camera {
             origin: origin,
             lower_left_corner: lower_left_corner,
             horizontal: horizontal,
-            verticcal: vertical,
+            vertical,
         }
+    }
+
+    pub fn shoot_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -3,13 +3,25 @@ use std::{
     io::{BufWriter, Write},
 };
 
-pub fn write_color(out: &mut BufWriter<File>, pixel_color: crate::vec3::Vec3) {
+use rand::Rng;
+
+pub fn write_color<W: Write>(
+    out: &mut BufWriter<W>,
+    pixel_color: crate::vec3::Vec3,
+    samples_per_pixel: u32,
+) {
+    let scale = 1.0 / (samples_per_pixel as f64);
+
+    let r = pixel_color.x * scale;
+    let g = pixel_color.y * scale;
+    let b = pixel_color.z * scale;
+
     out.write_all(
         format!(
             "{} {} {}\n",
-            (255.999 * pixel_color.x).round() as i32,
-            (255.999 * pixel_color.y).round() as i32,
-            (255.999 * pixel_color.z).round() as i32
+            (256.0 * clamp(r, 0.0, 0.999)).round() as i32,
+            (256.0 * clamp(g, 0.0, 0.999)).round() as i32,
+            (256.0 * clamp(b, 0.0, 0.999)).round() as i32
         )
         .as_bytes(),
     )
@@ -30,4 +42,14 @@ pub fn rand() -> f64 {
 
 pub fn random_double(min: f64, max: f64) -> f64 {
     min + (max - min) * rand()
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        min
+    } else if x > max {
+        max
+    } else {
+        x
+    }
 }
